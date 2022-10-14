6f43ca3f1cdc3e0f6a10f76f208063761c3e3de4
Tom Wey <tom.wey@theguardian.com>
2022-10-14 15:59:03 +0100

    Returning the return value of a function counts as using it

--- src/rule.ts
+++ src/rule.ts
@@ -1,77 +1,99 @@
-import { ESLintUtils } from '@typescript-eslint/utils';
-import {AST_NODE_TYPES, CallExpression, FunctionDeclaration, FunctionExpression, TSDeclareFunction, TSEmptyBodyFunctionExpression} from "@typescript-eslint/types/dist/generated/ast-spec";
-import { DefinitionType, FunctionNameDefinition, Reference } from '@typescript-eslint/scope-manager';
-import {TSESTree} from "@typescript-eslint/types";
+import { ESLintUtils } from "@typescript-eslint/utils";
+import {
+  AST_NODE_TYPES,
+  CallExpression,
+  FunctionDeclaration,
+  FunctionExpression,
+  TSDeclareFunction,
+  TSEmptyBodyFunctionExpression,
+} from "@typescript-eslint/types/dist/generated/ast-spec";
+import {
+  DefinitionType,
+  FunctionNameDefinition,
+  Reference,
+  Scope,
+} from "@typescript-eslint/scope-manager";
 
 const createRule = ESLintUtils.RuleCreator(
-    name => `https://example.com/rule/${name}`,
+  (name) => `https://example.com/rule/${name}`
 );
 
 const isFunctionCalled = (ref: Reference): boolean =>
-    ref.identifier.type === AST_NODE_TYPES.Identifier && ref.identifier.parent.type === AST_NODE_TYPES.CallExpression;
+  ref.identifier.type === AST_NODE_TYPES.Identifier &&
+  ref.identifier.parent.type === AST_NODE_TYPES.CallExpression;
 
 const getCallExpression = (ref: Reference): CallExpression | undefined => {
-    if (isFunctionCalled(ref)) {
-        return ref.identifier.parent as CallExpression;
-    }
-}
+  if (isFunctionCalled(ref)) {
+    return ref.identifier.parent as CallExpression;
+  }
+};
 
 const isReturnValueUsed = (callExpr: CallExpression): boolean => {
-    return callExpr.parent.type === AST_NODE_TYPES.VariableDeclarator;
-}
+  console.log(callExpr);
+  return (
+    callExpr.parent.type === AST_NODE_TYPES.VariableDeclarator ||
+    callExpr.parent.type === AST_NODE_TYPES.ReturnStatement
+  );
+};
 
-const hasNonVoidReturnType = (node: FunctionDeclaration | FunctionExpression | TSDeclareFunction | TSEmptyBodyFunctionExpression): boolean =>
-    // If no returnType is declared then we cannot run this rule
-    node.returnType && node.returnType.typeAnnotation.type !== AST_NODE_TYPES.TSVoidKeyword;
+const hasNonVoidReturnType = (
+  node:
+    | FunctionDeclaration
+    | FunctionExpression
+    | TSDeclareFunction
+    | TSEmptyBodyFunctionExpression
+): boolean =>
+  // If no returnType is declared then we cannot run this rule
+  node.returnType &&
+  node.returnType.typeAnnotation.type !== AST_NODE_TYPES.TSVoidKeyword;
 
 export const rule = createRule({
-    create(context) {
-        return {
-            Program(): void {
-                const scope = context.getScope();
-                scope.references.map(ref => {
-                    // TODO - why is defs an array?
-                    const functions = ref.resolved.defs.filter(def => def.type === DefinitionType.FunctionName) as FunctionNameDefinition[];
-                    functions.forEach(fun => {
-                        const nonVoid = hasNonVoidReturnType(fun.node);
-                        if (nonVoid) {
-                            const maybeCallExpression = getCallExpression(ref);
-                            if (maybeCallExpression && !isReturnValueUsed(maybeCallExpression)) {
-                                context.report({
-                                    messageId: 'unused',
-                                    node: ref.identifier,
-                                });
-                            }
-                        }
-                    })
-                })
-            },
-            // CallExpression(node: CallExpression) {
-            //     console.log(node.callee)
-            // },
-            // FunctionDeclaration(node: FunctionDeclaration) {
-            //     if (hasNonVoidReturnType(node)) {
-            //         node.
-            //         context.report({
-            //             messageId: 'unused',
-            //             node: node.id,
-            //         });
-            //     }
-            // },
-        };
+  create(context) {
+    const traverseScope = (scope: Scope): void => {
+      scope.childScopes.forEach((childScope) => traverseScope(childScope));
+
+      scope.references.map((ref) => {
+        // TODO - why is defs an array?
+        const functions = ref.resolved.defs.filter(
+          (def) => def.type === DefinitionType.FunctionName
+        ) as FunctionNameDefinition[];
+
+        functions.forEach((fun) => {
+          const nonVoid = hasNonVoidReturnType(fun.node);
+          if (nonVoid) {
+            const maybeCallExpression = getCallExpression(ref);
+            if (
+              maybeCallExpression &&
+              !isReturnValueUsed(maybeCallExpression)
+            ) {
+              context.report({
+                messageId: "unused",
+                node: ref.identifier,
+              });
+            }
+          }
+        });
+      });
+    };
+
+    return {
+      Program(): void {
+        const scope = context.getScope();
+        traverseScope(scope);
+      },
+    };
+  },
+  name: "no-unused-return-value",
+  meta: {
+    docs: {
+      description: "Rule to catch unused function return values",
+      recommended: "warn",
     },
-    name: 'no-unused-return-value',
-    meta: {
-        docs: {
-            description:
-                'Rule to catch unused function return values',
-            recommended: 'warn',
-        },
-        messages: {
-            unused: 'Use the return value',
-        },
-        type: 'suggestion',
-        schema: [],
+    messages: {
+      unused: "Use the return value",
     },
-    defaultOptions: [],
+    type: "suggestion",
+    schema: [],
+  },
+  defaultOptions: [],
 });
